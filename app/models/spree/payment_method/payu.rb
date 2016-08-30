e2e73d5d553657951dad6a10bb0f9e542b5c425c
Michał Podlecki <m.podlecki@op.pl>
2016-08-30 12:26:44 +0200

    add #compute_commission method to get commisson's value

--- app/models/spree/payment_method/payu.rb
+++ app/models/spree/payment_method/payu.rb
@@ -26,13 +26,21 @@ module Spree
     def create_adjustment(payment)
       return unless payment.new_record?
 
-      compute_charge = Rails.application.config.payu_charge if defined?(Rails)
-
       payment.order.adjustments.create!(
         amount: compute_charge.call(payment.order),
         label: I18n.t(:charge_label, scope: :payu),
         order: payment.order
       )
+    end
+
+    def compute_commission(order)
+      compute_charge.call(order)
+    end
+
+    private
+
+    def compute_charge
+      Rails.application.config.payu_charge if defined?(Rails)
     end
   end
 end
